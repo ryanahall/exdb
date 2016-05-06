6536971a0b9a50d0ce663734180471e2c9834c53
Ryan Hall <rhall@zenefits.com>
2016-05-05 22:07:38 -0700

    Handle closed connections

--- lib/exdb.ex
+++ lib/exdb.ex
@@ -1,51 +1,67 @@
 defmodule Exdb do
-    use Application
+  use Application
+  require Logger
 
-    @doc false
-    def start(_type, _args) do
-        import Supervisor.Spec
+  @port 4040
 
-        #Exdb.Server.start_link()
+  @doc false
+  def start(_type, _args) do
+    import Supervisor.Spec
 
-        children =
-            [
-                supervisor(Task.Supervisor, [[name: Exdb.TaskSupervisor]]),
-                worker(Task, [Exdb, :accept, [4040]])
-            ]
+    #Exdb.Server.start_link()
 
-        opts = [strategy: :one_for_one, name: Exdb.Supervisor]
-        Supervisor.start_link(children, opts)
-    end
+    Logger.info "Starting ExDB"
 
-    def accept(port) do
-        case :gen_tcp.listen(port, [:binary, packet: :line, active: false]) do
-            {:ok, socket} ->
-                case IO.puts "listening on port #{port}\n" do
-                    _ -> loop_acceptor(socket)
-                end
-            {:error, _} -> IO.puts "unable to listen on port #{port}\n"
+    children =
+      [
+        supervisor(Task.Supervisor, [[name: Exdb.TaskSupervisor]]),
+        worker(Task, [Exdb, :accept, [@port]])
+      ]
+
+    opts = [strategy: :one_for_one, name: Exdb.Supervisor]
+    Supervisor.start_link(children, opts)
+  end
+
+  def accept(port) do
+    case :gen_tcp.listen(port, [:binary, packet: :line, active: false]) do
+      {:ok, socket} ->
+        case Logger.info "Listening on port #{port}" do
+          _ -> loop_acceptor(socket)
         end
+      {:error, _} -> Logger.error "Unable to listen on port #{port}\n"
     end
+  end
 
-    defp loop_acceptor(socket) do
-        case :gen_tcp.accept(socket) do
-            {:ok, client} -> Task.Supervisor.start_child(Exdb.TaskSupervisor, fn -> serve(client) end)
-            {:error, :closed} -> {:error, "error while reading socket: socket was closed\n"}
-            {:error, _} -> IO.puts "unable to accept client on socket\n"
+  defp loop_acceptor(socket) do
+    case :gen_tcp.accept(socket) do
+      {:ok, client} ->
+        case :inet.peername(client) do
+            {:ok, {host, port}} ->
+              addr = :inet_parse.ntoa(host) |> List.to_string
+              Logger.info "Accepted connection from host #{addr}:#{port}"
+              Task.Supervisor.start_child(Exdb.TaskSupervisor, fn -> serve(client) end)
+            {:error, _} -> Logger.error "Unable to determine peername"
         end
-
-        loop_acceptor(socket)
+      {:error, :closed} -> Logger.error "Error while reading socket: socket was closed"
+      {:error, _} -> Logger.error "Unable to accept client on socket"
     end
 
-    defp serve(socket) do
-        {status, msg} =
-            case :gen_tcp.recv(socket, 0) do
-                {:ok, line} -> {:ok, "received msg: #{line}\n"}
-                {:error, :closed} -> {:error, "error while reading socket: socket was closed\n"}
-                {:error, _} -> {:error, "error while reading socket\n"}
-            end
+    loop_acceptor(socket)
+  end
 
-        :gen_tcp.send(socket, msg)
-        serve(socket)
+  defp serve(socket) do
+    result =
+      case :gen_tcp.recv(socket, 0) do
+        {:ok, line} -> {:ok, "received msg: #{line}\n"}
+        {:error, reason} -> {:error, reason}
+      end
+
+    case result do
+      {:ok, msg} ->
+         :gen_tcp.send(socket, msg)
+         serve(socket)
+      {:error, :closed} -> Logger.info "Closed connection from host"
+      {:error, reason} -> serve(socket)
     end
+  end
 end
